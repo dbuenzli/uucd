8bd0374ed99c7f6196242c43775dc14adb582c49
Daniel Bünzli <daniel.buenzli@erratique.ch>
2020-02-24 21:17:23 +0100

    Fix copyrights.

--- src/uucd.ml
+++ src/uucd.ml
@@ -1,5 +1,5 @@
 (*---------------------------------------------------------------------------
-   Copyright (c) 2012 Daniel C. Bünzli. All rights reserved.
+   Copyright (c) 2012 The uucd programmers. All rights reserved.
    Distributed under the ISC license, see terms at the end of the file.
    %%NAME%% %%VERSION%%
   ---------------------------------------------------------------------------*)
@@ -2899,7 +2899,7 @@ with
 | Failure e -> `Error e | Xmlm.Error (_, e) -> `Error (Xmlm.error_message e)
 
 (*---------------------------------------------------------------------------
-   Copyright (c) 2012 Daniel C. Bünzli
+   Copyright (c) 2012 The uucd programmers
 
    Permission to use, copy, modify, and/or distribute this software for any
    purpose with or without fee is hereby granted, provided that the above
--- src/uucd.mli
+++ src/uucd.mli
@@ -1,5 +1,5 @@
 (*---------------------------------------------------------------------------
-   Copyright (c) 2012 Daniel C. Bünzli. All rights reserved.
+   Copyright (c) 2012 The uucd programmers. All rights reserved.
    Distributed under the ISC license, see terms at the end of the file.
    %%NAME%% %%VERSION%%
   ---------------------------------------------------------------------------*)
@@ -1240,7 +1240,7 @@ let u_1F42B_gc = Uucd.cp_prop ucd 0x1F42B Uucd.general_category
 *)
 
 (*---------------------------------------------------------------------------
-   Copyright (c) 2012 Daniel C. Bünzli
+   Copyright (c) 2012 The uucd programmers
 
    Permission to use, copy, modify, and/or distribute this software for any
    purpose with or without fee is hereby granted, provided that the above
